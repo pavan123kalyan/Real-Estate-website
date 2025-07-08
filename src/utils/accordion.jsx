edc766c9ef3172d9a288c97e41f75516002d767c
pavan <paidipellipavan@gmail.com>
2025-07-09 00:57:52 +0530

    added

---
+++ src/utils/accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Best interest rates on the market",
+    detail:
+      "Take advantage of the best interest rates on the market with customized financing solutions that make owning your dream property easier and more affordable than ever.",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Prevent unstable prices",
+    detail:
+      "Protect your investment with smart property choices that shield you from market fluctuations and ensure long-term value stability.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best price on the market",
+    detail:
+      "Get unbeatable value with the best prices on the market, offering premium properties at rates that perfectly balance quality, comfort, and affordability.",
+  },
+];
+export default data;
