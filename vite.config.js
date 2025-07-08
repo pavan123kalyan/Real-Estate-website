37060ddc7a0426da2698430ec8eb6c8d485b3f8f
pavan <paidipellipavan@gmail.com>
2025-07-09 01:19:14 +0530

    added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Real-Estate-website/',  // <-- Add this line
   plugins: [react()],
 })
